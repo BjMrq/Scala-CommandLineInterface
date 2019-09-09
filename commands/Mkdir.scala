2e020e23477e7f22550d37585953742ea300a3dc
bjmrq <bmarquiscom@gmail.com>
2019-09-09 18:35:29 -0400

    Start mkdir implementation

--- commands/Mkdir.scala
+++ commands/Mkdir.scala
@@ -1,7 +1,25 @@
 package com.rtjvm.scala.oop.commands
 
+import com.rtjvm.scala.oop.files.Directory
 import com.rtjvm.scala.oop.filesystem.State
 
 class Mkdir(name: String) extends Command {
-  override def apply(state: State): State = ???
+  override def apply(state: State): State = {
+    val wd = state.wd
+    if (wd.hasEntry(name)){
+      state.setMessage("Entry " + name + " already exist")
+    } else if (checkIllegal(name)){
+      state.setMessage(name + " illegal entry name")
+    } else {
+      doMkdir(state, name)
+    }
+  }
+
+  def checkIllegal(name: String): Boolean = {
+    name.contains(".") || name.contains(Directory.SEPARATOR)
+  }
+
+  def doMkdir(state: State, name: String): State = {
+    
+  }
 }
